9eb5465c59b40b4f04077bc80662059d7c6d5ac0
alexandre-imbernon <alexandre.imbernon@laplateforme.io>
2024-05-23 11:16:58 +0200

    addition of the GET method to retrieve all books in the db

--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -3,6 +3,7 @@ const Book = require('../models/bookSchema');
 
 const router = express.Router();
 
+// Route pour ajouter un livre
 router.post('/addBook', async (req, res) => {
     const { title, author, image, status, pages, category } = req.body;
     try {
@@ -19,6 +20,16 @@ router.post('/addBook', async (req, res) => {
         res.status(201).json({ message: 'Livre ajouté avec succès.' });
     } catch (error) {
         res.status(400).send({ success: false, message: error.message });
+    }
+});
+
+// Route pour récupérer tous les livres
+router.get('/books', async (req, res) => {
+    try {
+        const books = await Book.find();
+        res.status(200).json(books);
+    } catch (error) {
+        res.status(500).send({ success: false, message: error.message });
     }
 });
 
