ed6f76101fef63e344e20e7ae492e32a1cea6944
Adel Grim <adel.grim@laplateforme.iogit config --global adel.grim@laplateforme.io>
2024-05-24 11:38:08 +0200

    add a fetch on homepage

--- frontend/book-tracker/src/pages/AddBookPage.js
+++ frontend/book-tracker/src/pages/AddBookPage.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import BookForm from '../components/BookForm';
 
-
-
-
 function AddBookPage({ addBook }) {
+    useEffect(() => {
+        async function postBooks(addBook) {
+            try {
+                const response = await fetch('http://localhost:5000/api/addBook');
+                const data = await response.json();
+
+                console.log(data);
+            } catch (error) {
+                console.error('Erreur lors de la récupération de tous les livres', error);
+            }
+        }
+
+        postBooks();
+    }, []);
+
   return (
     <div>
       <h2>Ajouter un livre</h2>
--- frontend/book-tracker/src/pages/HomePage.js
+++ frontend/book-tracker/src/pages/HomePage.js
@@ -1,11 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import BookList from '../components/BookList';
 import SearchBar from '../components/SearchBar';
-
-
-
-
-
 
 function HomePage({ books, selectBook }) {
   const [searchQuery, setSearchQuery] = useState('');
@@ -16,10 +11,35 @@ function HomePage({ books, selectBook }) {
     (filter === '' || book.category === filter)
   );
 
+    const [Boooks, setBooks] = useState([]);
+
+    useEffect(() => {
+        async function getAllBooks() {
+            try {
+                const response = await fetch('http://localhost:5000/api/books');
+                const data = await response.json();
+
+                setBooks(data)
+                console.log(data);
+            } catch (error) {
+                console.error('Erreur lors de la récupération de tous les livres', error);
+            }
+        }
+
+        getAllBooks();
+    }, []);
+
   return (
     <div>
       <SearchBar setSearchQuery={setSearchQuery} setFilter={setFilter} />
       <BookList books={filteredBooks} selectBook={selectBook} />
+        {Boooks.map((bokk, index) => (
+            <div>
+                <h3 key={index}>{bokk.title}</h3>
+                <p>{bokk.author}</p>
+                <p>nombre de pages totales :{bokk.pages} pages</p>
+            </div>
+        ))}
     </div>
   );
 }
